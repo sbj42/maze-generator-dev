8fa481f7c94f7b1f7f864d0cc561aae1316e525e
James Clark <sbj@dimins.com>
2016-10-10 23:16:41 -0400

    Switch web demo to canvas

--- src/web-demo-main.js
+++ src/web-demo-main.js
@@ -1,35 +1,55 @@
 /* eslint-env browser */
 var algorithmFunc = require('mg-algorithm');
 var Maze = require('@sbj42/maze-generator-core').Maze;
+var dirs = require('@sbj42/maze-generator-core').directions;
+
+function makeRandom(seed) {
+    var randomjs = require('random-js');
+    var engine = randomjs.engines.mt19937().seed(seed);
+    var real = randomjs.real(0, 1);
+    return function() {
+        return real(engine);
+    };
+}
 
 var go = document.getElementById('go');
 go.addEventListener('click', function() {
     var width = +document.getElementById('width').value;
     var height = +document.getElementById('height').value;
+    var seed = +document.getElementById('height').value;
+    var zoom = 4;
     if (!width || !height || width < 1 || height < 1)
         return;
 
     var options = {
-        random: Math.random
+        random: seed ? makeRandom(seed) : Math.random
     };
 
     var maze = new Maze(width, height);
     algorithmFunc(maze, options);
 
-    var html = '';
-    var x, y;
-    for (x = 0; x < maze.width(); x ++) {
-        html += '__';
-    }
-    html += '_';
-    html += '\n';
+    var canvas = document.getElementById('result');
+    canvas.width = width * (1 + zoom) + 1;
+    canvas.height = height * (1 + zoom) + 1;
+    var context = canvas.getContext('2d');
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'black';
+    var cy, y;
     for (y = 0; y < maze.height(); y ++) {
+        cy = y * (1 + zoom);
+        var cx, x;
         for (x = 0; x < maze.width(); x ++) {
-            html += maze.cell(x, y).west() ? '_' : '|';
-            html += maze.cell(x, y).south() ? ' ' : '_';
+            cx = x * (1 + zoom);
+            context.fillRect(cx, cy, 1, 1);
+            if (!maze.getPassage(x, y, dirs.NORTH))
+                context.fillRect(cx + 1, cy, zoom, 1);
+            if (!maze.getPassage(x, y, dirs.WEST))
+                context.fillRect(cx, cy + 1, 1, zoom);
         }
-        html += maze.cell(maze.width()-1, y).east() ? ' ' : '|';
-        html += '\n';
+        cx = x * (1 + zoom);
+        context.fillRect(cx, cy, 1, 1 + zoom);
     }
-    document.getElementById('result').innerHTML = html;
+    cy = y * (1 + zoom);
+    context.fillRect(0, cy, canvas.width, 1);
 });
